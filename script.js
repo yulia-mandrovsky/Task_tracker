53496f02112e04d2807d82d47312347ae5129d48
YuliaS <ysolodukha@gmail.com>
2021-03-26 17:55:34 +0300

    some comment

--- script.js
+++ script.js
@@ -1,44 +1,70 @@
 
+// добавление записи
 let block = document.querySelector('.note');
 const submitBtn = document.querySelector('.submit') 
 const sortImage = document.querySelector('.sort-image')
 
 submitBtn.addEventListener('click', () => {
     let block2 = block.cloneNode(true);
-    console.log(block2.querySelector('.input-for-note'))
+    // console.log(block2.querySelector('.input-for-note'))
     block2.querySelector('.input-for-note').value = '';
     block.after(block2);
 })
 
+// кнопка удаления 
 
-const sortButton = document.querySelector('.sort-image')
+let removeBtn = document.querySelectorAll('.remove-btn');
 
-// // const headerName = document.querySelector('.header-name');
-const notes = document.querySelectorAll(('input').value);
-// // const rows = document.querySelectorAll('tr');
-const frame = document.querySelector('.frame');
-// // const table = document.querySelector('table');
+removeBtn.addEventListener('click', (event) => {
+    console.log(uhiuh);
+    // remove(this.document.querySelector('.note'));
+});
 
-let flagName = true;
-console.log(notes)
-console.log(frame.notes)
+// greenBox.addEventListener('mouseover', (event) => {
+//     event.target.style.backgroundColor = 'yellow';
+// });
 
-headerName.addEventListener('click', (event) => {
-   if (flagName == true) {
-//       headerName.innerHTML = 'Name ˅';
-//       let sortedNameRows = Array.from(table.rows)
-//          .slice(1)
-//          .sort((rowA, rowB) => 
-//          rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
-//          table.tBodies[0].append( ...sortedNameRows);
+
+
+
+// // сортировка
+// const sortButton = document.querySelector('.for-sort');
+
+// const frame = document.querySelector('.frame');
+// const notes = document.querySelectorAll('.note');
+
+// console.log(notes)
+
+
+// function forArr(notes) {
+//     for (let i = 0; i < notes.length; i++) {
+//         // const el = notes[i];
+//         arrNotes.push(el)
+//         return arrNotes;
+//     }
+// }
+
+
+
+
+// let flagName = true;
+
+// sortButton.addEventListener('click', (event) => {
+//    if (flagName == true) {
+//         document.querySelector('.sort-image').getElementsByClassName.display = 'none';
+//       let sortedNotes = Array.from(frame.notes)
+//       console.log(sortedNotes)
+//         .sort((noteA, noteB) => 
+//          noteA.value > noteB.value ? 1 : -1);
+//          frame[0].append( ...sortedNotes);
+//          console.log(hgjkh)
 //          flagName = false;
 //    } else {
-//       headerName.innerHTML = 'Name ˄';
-//          let sortedNameRows = Array.from(table.rows)
-//             .slice(1)
-//             .sort((rowA, rowB) => 
-//             rowA.cells[0].innerHTML < rowB.cells[0].innerHTML ? 1 : -1);
-//          table.tBodies[0].append( ...sortedNameRows);
+// //       headerName.innerHTML = 'Name ˄';
+//          let sortedNotes = Array.from(frame.notes)
+//             .sort((noteA, noteB) => 
+//             noteA.value < noteB.value ? 1 : -1);
+//          frame[0].append( ...sortedNotes);
 //          flagName = true;
 //    }
 // });
