2770d469ff27392b133bab5ca4d98997c890ac5a
YuliaS <ysolodukha@gmail.com>
2021-03-29 15:19:14 +0300

    finally but dnd don't work

--- script.js
+++ script.js
@@ -1,71 +1,54 @@
 
 // добавление записи
+
 let block = document.querySelector('.note');
+const mainOriginalBlock = block;
 const submitBtn = document.querySelector('.submit') 
 const sortImage = document.querySelector('.sort-image')
+let removeBtns = document.querySelectorAll('.remove-btn');
+const listToDo = document.querySelector('.myUL');
 
-submitBtn.addEventListener('click', () => {
-    let block2 = block.cloneNode(true);
-    // console.log(block2.querySelector('.input-for-note'))
-    block2.querySelector('.input-for-note').value = '';
-    block.after(block2);
-})
-
-// кнопка удаления 
-
-let removeBtn = document.querySelectorAll('.remove-btn');
-
-removeBtn.addEventListener('click', (event) => {
-    console.log(uhiuh);
-    // remove(this.document.querySelector('.note'));
+const btn = document.querySelector('.remove-btn')
+btn.addEventListener('click', (event) => {
+    console.log('kjnjljnlkm')
+    block.remove();
 });
 
-// greenBox.addEventListener('mouseover', (event) => {
-//     event.target.style.backgroundColor = 'yellow';
-// });
 
-
-
+submitBtn.addEventListener('click', () => {
+    let block2 = mainOriginalBlock.cloneNode(true);
+    block2.querySelector('.input-for-note').value = '';
+    listToDo.appendChild(block2);
+    const btn = block2.querySelector('.remove-btn')
+    btn.addEventListener('click', (event) => {
+        console.log('kjnjljnlkm')
+        block2.remove();
+    });
+})
 
 // // сортировка
-// const sortButton = document.querySelector('.for-sort');
 
-// const frame = document.querySelector('.frame');
-// const notes = document.querySelectorAll('.note');
+const sortButton = document.querySelector('.for-sort');
+const sortImg1 = document.querySelector('.sort-image1');
+const sortImg2 = document.querySelector('.sort-image2');
 
-// console.log(notes)
+let flagName = true;
 
+sortButton.addEventListener('click', (event) => {
+    const inputNotes = document.querySelectorAll('.input-for-note');
+    const inputArray = Array.prototype.slice.call(inputNotes)
+    inputArray.sort((el1, el2) => {
+        return el1.value.localeCompare(el2.value) * (flagName ? -1 : 1);
+    })
+    flagName = !flagName;
+    inputArray.forEach((input) => {
+        const parentElement = input.parentElement;
+        parentElement.remove();
+        listToDo.appendChild(parentElement)
+    })
+    sortImg1.classList.toggle('display-none');
+    sortImg2.classList.toggle('display-none');
+});
 
-// function forArr(notes) {
-//     for (let i = 0; i < notes.length; i++) {
-//         // const el = notes[i];
-//         arrNotes.push(el)
-//         return arrNotes;
-//     }
-// }
-
-
-
-
-// let flagName = true;
-
-// sortButton.addEventListener('click', (event) => {
-//    if (flagName == true) {
-//         document.querySelector('.sort-image').getElementsByClassName.display = 'none';
-//       let sortedNotes = Array.from(frame.notes)
-//       console.log(sortedNotes)
-//         .sort((noteA, noteB) => 
-//          noteA.value > noteB.value ? 1 : -1);
-//          frame[0].append( ...sortedNotes);
-//          console.log(hgjkh)
-//          flagName = false;
-//    } else {
-// //       headerName.innerHTML = 'Name ˄';
-//          let sortedNotes = Array.from(frame.notes)
-//             .sort((noteA, noteB) => 
-//             noteA.value < noteB.value ? 1 : -1);
-//          frame[0].append( ...sortedNotes);
-//          flagName = true;
-//    }
-// });
+// drag-n-drop
 
